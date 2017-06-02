fcccec19b632fdbcbc17dfb675ff26c61ad3d00e
henrique moura <h3dema@users.noreply.github.com>
2017-06-02 09:16:53 -0300

    add Disclaimer

--- heap.py
+++ heap.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+ * priority-queue, Copyright (c) 2016-2017, Henrique Moura (h3dema)
+ * All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this
+ * software, please contact damacedo@dcc.ufmg.br.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ * 4. Neither the name of author nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE H3DEMA AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+"""
 """
 
-HEAP:  implements a max heap
+This python module implements two classes:
 
-MaxPriorityQueue : implements a max-priority queue with HEAP
+* MaxHeap:  implements a max heap
+* MaxPriorityQueue : implements a max-priority queue with HEAP
 
 """
 
